2ee93fffe9faf8e3d9adb9068634456dbdc55eb2
Shiva Ram Dam <41049081+ShivaRamDam11@users.noreply.github.com>
2018-12-10 21:25:41 +0545

    Add files via upload

---
+++ VariableByteEncoding.cpp
@@ -0,0 +1,94 @@
+#include<stdio.h>
+int decToBinary(int n) 
+{ 
+    for (int i = 7; i >= 0; i--) 
+	{ 
+        int k = n >> i; 
+        if (k & 1) 
+            printf("1"); 
+        else
+            printf("0"); 
+    } 
+    printf("\n");
+} 
+  
+
+main()
+{
+	long int i,j, ID[100], num, gap[100],r[100][100],q[100];
+	printf("enter how many DocIDs?\n");
+	scanf("%ld",&num);
+	printf("\nEnter the DocIds\n");
+	for(i=0;i<num;i++)
+	{
+		scanf("%ld",&ID[i]);
+	}
+	
+	// for finding gap values
+	gap[0]=ID[0];
+	for(i=1;i<num;i++)
+	{
+		gap[i]=ID[i]-ID[i-1];
+	}
+	
+	//for displaying gap values
+	printf("\nGap value in decimal value:\n");
+	for(i=0;i<num;i++)
+	{
+		printf("%ld\t",gap[i]);
+	}
+	
+	printf("\n");
+	
+	//for processing to find the VB codes
+	for(i=0;i<num;i++)
+	{
+		j=0;
+		if(gap[i]<128)
+		{
+			r[i][j]=gap[i];
+		}
+		
+		else
+		{
+			while(gap[i]>127)
+			{
+				r[i][j]=gap[i]%128;			
+				q[i]=gap[i]/128;
+				gap[i]=q[i];
+				j++;
+				if (q[i]<128)
+					break;
+			}
+		}		
+	}
+	
+	// for printing variable byte codes in decimal value from forward
+	printf("\nVB code in decimal value:\n");
+	for(i=0;i<num;i++)
+	{
+		int n=q[i];
+		decToBinary(n);
+		for(j=i;j>=0;j--)
+		{
+			if(j==0)
+			{
+				r[i][j]+=128;
+				n=r[i][j];				
+			}
+			else
+				n=r[i][j];
+				
+			if(n!=0)
+				decToBinary(n);
+		}
+		printf("\n");
+	}
+}
+
+
+
+
+
+
+
